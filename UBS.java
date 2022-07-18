1d9c069ec42094c4bdf33643b3b5217046446110
Stefany Souza <stefanyraiissa24@gmail.com>
2022-07-18 17:55:06 -0300

    Projeto realizado para conclusão do primeiro modulo turma generation

---
+++ UBS.java
@@ -0,0 +1,12 @@
+package ProjetoUBS;
+
+public interface UBS {
+	
+	// MÉTODOS DA INTERFACE
+			
+		void temperatura(double testeTemperatura);
+		void avisoTemperatura();
+		void cartaoVac(boolean CartaoVac);
+			
+}
+
